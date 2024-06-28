806c9b4a091a6ff9682c6383abbcda9f359ce803
adam n <3193391+bcuz@users.noreply.github.com>
2024-06-28 10:52:21 -0400

    diff js server

--- comments.js
+++ comments.js
@@ -1,16 +1,33 @@
 // Create web server
-var http = require('http');
-var fs = require('fs');
+// 1. Create a web server
+// 2. Define a route for GET /comments
+// 3. Return the list of comments
+// 4. Start the web server
 
-// Create a server
-http.createServer(function (req, res) {
-    // Read file
-    fs.readFile('comments.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080); // Server listen on port 8080
+const http = require('http');
 
-// Print message in console
-console.log('Server running at http://
+const comments = [
+  { body: 'comment 1', postId: 1 },
+  { body: 'comment 2', postId: 1 },
+  { body: 'comment 3', postId: 2 },
+  { body: 'comment 4', postId: 2 },
+];
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(comments));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Run the code, open the browser, and visit http://localhost:3000/comments
+// You should see the list of comments
+// If you visit any other URL, you should see a 404 response
+
